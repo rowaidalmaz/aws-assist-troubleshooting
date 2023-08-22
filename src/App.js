a5cab28e44116a678a17ada6d91e982095bef0f2
rowaidalmaz <52274424+rowaidalmaz@users.noreply.github.com>
2023-08-22 12:00:20 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import background from './background.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+        <img src={background} alt="logo" width="1500" height="1000" />
+       
+
+    </div>
+  );
+}
+
+export default App;
